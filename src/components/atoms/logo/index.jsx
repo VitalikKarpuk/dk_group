8d7fedd18b9a677b2ba237243e77004fd8a8d27e
Vitali Karpuk <vitalikkarpuk652@gmail.com>
2025-05-23 14:04:20 +0300

    fix

--- src/components/atoms/logo/index.jsx
+++ src/components/atoms/logo/index.jsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import logo from "./mainLogo.jpg";
+import logo from "./logo.png";
 
 import style from "./index.module.css";
 import { Link } from "react-router-dom";
