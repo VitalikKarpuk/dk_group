881333e048b09921d3414efce20f620a842b1fc4
Vitali Karpuk <vitalikkarpuk652@gmail.com>
2025-05-23 14:37:37 +0300

    test

--- src/components/atoms/logo/index.jsx
+++ src/components/atoms/logo/index.jsx
@@ -1,13 +1,13 @@
-// import classNames from "classnames";
-// import logo from "./img/logo.png";
+import classNames from "classnames";
+import logo from "./logo.png";
 
-// import style from "./index.module.css";
+import style from "./index.module.css";
 import { Link } from "react-router-dom";
 
 const Logo = ({ className }) => {
   return (
     <Link to={"/"}>
-      {/* <img src={logo} alt="" className={classNames(style.logo, className)} /> */}
+      <img src={logo} alt="" className={classNames(style.logo, className)} />
     </Link>
   );
 };
